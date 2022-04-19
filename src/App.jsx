9fcbd6d55a2489930a32d693c63217eb7b2b237c
Kaizercode <69699616+Kaizercode@users.noreply.github.com>
2022-04-19 11:37:43 +0100

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
+
+   const address = useAddress();
+   const connectWithMetamask = useMetamask();
+   console.log("👋 Address:", address);
+
+   if (!address) {
+     return (
+       <div className="landing">
+         <h1>Welcome to GreenDAO</h1>
+         <button onClick={connectWithMetamask} className="btn-hero">
+           Connect your wallet
+         </button>
+       </div>
+     );
+   }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
 };
